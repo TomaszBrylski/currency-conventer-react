19635cd4b3377af6e9d5154cfe946c2a9f3628c2
Tomasz Brylski <brylskitomasz@gmail.com>
2024-02-04 21:51:30 +0100

    Changed URL

--- src/App/Form/useRatesData.js
+++ src/App/Form/useRatesData.js
@@ -1,36 +1,37 @@
 import { useEffect, useState } from "react";
 
 export const useRatesData = () => {
-    const [ratesData, setRatesData] = useState({
-        state: "loading",
-    });
+  const [ratesData, setRatesData] = useState({
+    state: "loading",
+  });
 
-    useEffect(() => {
-        const fetchRates = async () => {
-            try {
-                const response = await fetch("cur_live_LO7p2dI7bDzYx6BoNIu3lkBEizY7erOuZRLC3pxW")
+  useEffect(() => {
+    const fetchRates = async () => {
+      try {
+        const response = await fetch(
+          "https://api.currencyapi.com/v3/latest?apikey=cur_live_LO7p2dI7bDzYx6BoNIu3lkBEizY7erOuZRLC3pxW&currencies=EUR%2CUSD%2CCAD&base_currency=PLN"
+        );
 
-                if (!response.ok) {
-                    throw new Error(response.statusText);
-                }
+        if (!response.ok) {
+          throw new Error(response.statusText);
+        }
 
-                const { rates, date } = await response.json();
+        const { rates, date } = await response.json();
 
-                setRatesData({
-                    state: "success",
-                    rates,
-                    date,
-                });
+        setRatesData({
+          state: "success",
+          rates,
+          date,
+        });
+      } catch {
+        setRatesData({
+          state: "error",
+        });
+      }
+    };
 
-            } catch {
-                setRatesData({
-                    state: "error",
-                });
-            }
-        };
+    setTimeout(fetchRates, 1000);
+  }, []);
 
-        setTimeout(fetchRates, 1000);
-    }, []);
-
-    return ratesData;
-}
+  return ratesData;
+};
